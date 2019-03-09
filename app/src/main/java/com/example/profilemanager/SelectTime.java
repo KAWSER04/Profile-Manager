8307d2fa3d4543696be9d4473c9a6fb08782eef4
unknown <julfikercseru@gmail.com>
2019-03-09 23:27:13 +0600

    New Activities Added To SelectTime

--- app/src/main/java/com/example/profilemanager/SelectTime.java
+++ app/src/main/java/com/example/profilemanager/SelectTime.java
@@ -30,5 +30,23 @@ public class SelectTime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_time);
+
+        button1 = (Button) findViewById(R.id.startTimeButton);
+        button2 = (Button) findViewById(R.id.endtTimeButton);
+        // et_name = (EditText)findViewById(R.id.etname);
+        save = (Button) findViewById(R.id.saveButton);
+
+        textView1 = (TextView) findViewById(R.id.startTimeView);
+        textView2 = (TextView) findViewById(R.id.endTimeView);
+
+        saturday = (Switch) findViewById(R.id.saturday);
+        sunday = (Switch) findViewById(R.id.sunday);
+        monday = (Switch) findViewById(R.id.monday);
+        tuesday = (Switch) findViewById(R.id.tuesday);
+        wednesday = (Switch) findViewById(R.id.wednesday);
+        thursday = (Switch) findViewById(R.id.thursday);
+        friday = (Switch) findViewById(R.id.friday);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroupp);
     }
 }
