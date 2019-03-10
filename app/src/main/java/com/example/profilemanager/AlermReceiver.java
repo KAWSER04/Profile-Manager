e1c25d84273b41588bca444fd48e93c7a80b6057
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-03-11 01:13:46 +0600

    inside AlarmReceiver Class onReceiver method implemented

--- app/src/main/java/com/example/profilemanager/AlermReceiver.java
+++ app/src/main/java/com/example/profilemanager/AlermReceiver.java
@@ -1,8 +1,48 @@
 package com.example.profilemanager;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Vibrator;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AlermReceiver extends BroadcastReceiver {
 
-    
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        SqlHelper sqlHelper = new SqlHelper(context);
+
+        Vibrator vibrator = (Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(500);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        String time = dateFormat.format(date);
+
+        String[] times = time.split(":");
+        int hour = Integer.parseInt(times[0]);
+        int min = Integer.parseInt(times[1]);
+        time = String.valueOf(hour) + ":" + String.valueOf(min);
+
+        Log.i("myTag", "onReceive: "+sqlHelper.isSilent(time));
+
+        if(sqlHelper.isSilent(time)==0) {
+            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+        }else if(sqlHelper.isSilent(time)==1) {
+            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+        }
+
+
+
+
+    }
+
+
 }
