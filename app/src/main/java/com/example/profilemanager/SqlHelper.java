f45bab7cda86c59bdbb511f9deac103b63556c50
kawser <kawser.cse.ru@gmail.com>
2019-03-10 12:36:44 +0600

    declared column name variable

--- app/src/main/java/com/example/profilemanager/SqlHelper.java
+++ app/src/main/java/com/example/profilemanager/SqlHelper.java
@@ -1,4 +1,19 @@
 package com.example.profilemanager;
 
-public class SqlHelper {
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqlHelper extends SQLiteOpenHelper {
+
+    public static String db_name="events";
+    public static String table_name = "event_details";
+    public static String col_one = "id";
+    public static String col_two = "name";
+    public static String col_three = "start";
+    public static String col_four = "endtime";
+    public static String col_five = "days";
+    public static String col_six = "mode";
+    public static String col_seven = "session_key";
+    public static String col_eigth = "session_key_end";
+
+
 }
