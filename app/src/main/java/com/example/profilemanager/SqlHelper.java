d858ba0bd740b6b2e1911d3222f5456232f38dea
kawser <kawser.cse.ru@gmail.com>
2019-03-10 12:56:43 +0600

    contructor SqlHelper method added and on creat method added

--- app/src/main/java/com/example/profilemanager/SqlHelper.java
+++ app/src/main/java/com/example/profilemanager/SqlHelper.java
@@ -1,5 +1,7 @@
 package com.example.profilemanager;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SqlHelper extends SQLiteOpenHelper {
@@ -14,6 +16,27 @@ public class SqlHelper extends SQLiteOpenHelper {
     public static String col_six = "mode";
     public static String col_seven = "session_key";
     public static String col_eigth = "session_key_end";
+
+
+
+    public SqlHelper(Context context) {
+        super(context, db_name, null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table if not exists " + table_name  + " ( " + col_one + " integer primary key autoincrement , " +
+                col_two + " text, " + col_three + " text, " + col_four + " text, " +
+                col_five + " text, " + col_six + " integer, " + col_seven + " integer, " + col_eigth + " integer)";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
 
 
 }
