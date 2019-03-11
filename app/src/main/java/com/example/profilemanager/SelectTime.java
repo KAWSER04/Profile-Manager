68a7ae8207550ecc40da083205c0ee93610de9d7
kawser <kawser.cse.ru@gmail.com>
2019-03-11 13:07:38 +0600

    store in database

--- app/src/main/java/com/example/profilemanager/SelectTime.java
+++ app/src/main/java/com/example/profilemanager/SelectTime.java
@@ -10,6 +10,7 @@ import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -78,12 +79,24 @@ public class SelectTime extends AppCompatActivity {
                 checkButton(v);
                 swithButton();
                 ///db   here
+                StoreInDatabase();
                 Intent intent = new Intent(SelectTime.this,MainActivity.class);
                 startActivity(intent);
 
             }
         });
 
+    }
+
+
+    private void StoreInDatabase() {
+        String name = et_name.getText().toString().trim();
+        ItemsList itemsList = new ItemsList(1,name,startTime,endTime,days,mode,session_key_start,session_key_end);
+        if(sqlHelper.insert(itemsList)){
+            Toast.makeText(getApplicationContext(),"Insert successful",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(),"Failed to Insert",Toast.LENGTH_LONG).show();
+        }
     }
 
     private void swithButton() {
