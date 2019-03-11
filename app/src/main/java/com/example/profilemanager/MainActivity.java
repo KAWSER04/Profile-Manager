7d2c6a68599f3fb24363ff281c81d007932a85da
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-03-11 12:51:52 +0600

    list delete option added to custom adapter

--- app/src/main/java/com/example/profilemanager/MainActivity.java
+++ app/src/main/java/com/example/profilemanager/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Profile Scheduler");
+        getSupportActionBar().setTitle("Profile Manager");
 
         init_functions();
         sqlHelper = new SqlHelper(getApplicationContext());
